2d1a50e190ea5c9d609d73fe2420e1c8a1242b71
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-08-07 16:21:13 -0300

    feat: add first card info

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,10 +1,24 @@
 import { useTranslation } from 'react-i18next';
 import { PageCard } from '../../components/common';
+import myself from '../../images/pixel_cut.png';
 
 const Welcome = () => {
     const { t } = useTranslation();
 
-    return <PageCard>{t('welcome.whoAmI')}</PageCard>;
+    return (
+        <PageCard>
+            <div className="flex">
+                <img
+                    className="w-32 h-32 rounded-full border"
+                    src={myself}
+                    alt="image"
+                />
+                <div className="flex-initial text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+                    <h1 className="font-bold text-xl">{t('whoAmI')}</h1>
+                </div>
+            </div>
+        </PageCard>
+    );
 };
 
 export default Welcome;
