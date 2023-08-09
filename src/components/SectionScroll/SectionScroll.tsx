86b2eff9fac2fd390444419d3fa65a945fc69f51
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-08-09 19:40:12 -0300

    feat: remove styled components

--- src/components/SectionScroll/SectionScroll.tsx
+++ src/components/SectionScroll/SectionScroll.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react';
 import ballon from '../../images/ballon.png';
-import styled from 'styled-components';
-
-const Slider = styled.input`
-    &::-webkit-slider-thumb {
-        -webkit-appearance: none !important;
-        width: 15px !important;
-        height: 15px !important;
-        border: 1px solid black !important;
-    }
-`;
 
 const SectionScroll = () => {
     const [step, setStep] = useState(0);
     return (
         <div className="absolute bottom-[-64px] left-0 fixed h-full">
-            <Slider
+            <input
                 type="range"
                 className="h-[calc(100%-64px)] fixed"
                 id="sectionStep"
