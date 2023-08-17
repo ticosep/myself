e260be316de18b6c267115f11ad6fc9da2ef06d4
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2023-08-17 19:20:24 -0300

    fix: return urls

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <Router>
+        <Router basename="/">
             <Layout>
                 <App />
             </Layout>
